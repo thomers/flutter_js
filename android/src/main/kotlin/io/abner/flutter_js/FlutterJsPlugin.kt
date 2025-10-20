5fc4025028eb98dce0904c3e03b8c6fb5c6c81b8
Thomas Schweitzer <office@netzfrequenz.com>
2025-10-20 12:51:19 +0200

    Fixes for Flutter / Dart updates.

--- android/src/main/kotlin/io/abner/flutter_js/FlutterJsPlugin.kt
+++ android/src/main/kotlin/io/abner/flutter_js/FlutterJsPlugin.kt
@@ -9,7 +9,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.os.Looper
 import android.os.Handler
 // import fi.iki.elonen.NanoHTTPD
@@ -48,14 +47,6 @@ class FlutterJsPlugin : FlutterPlugin, MethodCallHandler {
     // depending on the user's project. onAttachedToEngine or registerWith must both be defined
     // in the same class.
     companion object {
-
-
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val instance = FlutterJsPlugin()
-            instance.onAttachedToEngine(registrar.context(), registrar.messenger())
-        }
-
         var jsEngineMap = mutableMapOf<Int, JSEngine>()
     }
 
